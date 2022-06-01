3c7c32644c86b6f330aff7ef4cb006adfc96ffd0
maivantien1107 <maivantien1107@users.noreply.github.com>
2022-06-01 15:01:33 +0700

    add code

---
+++ mvc/views/nav_bar.php
@@ -0,0 +1,26 @@
+<?php
+global $path_project;
+?>
+<div class="nav_bar">
+	<div class="logo">
+		<a href=<?php echo "/" . $path_project . "/" ?>> Group 11</a>
+	</div>
+	<nav id = "nav">
+			<ul id = "menuitem">
+				<li><a href=<?php echo "/" . $path_project . "/"?>><span>Trang chủ</span></a></li>
+				<li><a href=<?php echo "/" . $path_project . "/" . "about" ?>>Tin tức</a></li>
+				<li><a href=<?php echo "/" . $path_project . "/" . "products" ?>>Sản phẩm</a></li>
+				<li><a href=<?php echo "/" . $path_project . "/" . "contact" ?>>Liên hệ</a></li>
+				<li><a href=<?php echo "/" . $path_project . "/" . "login" ?>>Tài khoản</a></li>
+			</ul>
+
+	</nav>
+	<div class="cart-logo">
+		<a href=<?php echo "/" . $path_project . "/" . "cart" ?>><img src=<?php echo "/" . $path_project . "/" . "images/logo/cart.png" ?> width="30px"> &nbsp;</a>
+		<a href=<?php echo "/" . $path_project . "/" . "search" ?>><img src=<?php echo "/" . $path_project . "/" . "images/logo/searchbox.jpg" ?> width="30px"></a>
+		<!-- <div class="menu"> -->
+		<img src=<?php echo "/" . $path_project . "/" . "images/logo/menu.png" ?> alt="" class = "menu" onclick="menuToggle()" />
+		<!-- </div> -->
+	</div>
+</div>
+<script type="text/javascript" src = <?php echo "/" . $path_project . "/" . "public/javascript/nav_bar.js" ?>></script>
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$path_project = 'cnw-1';
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', $_SERVER['DOCUMENT_ROOT'] . DS . $path_project);
+
+require_once ROOT . DS . 'mvc' . DS . 'controllers' . DS . 'RouteController.php';
+$url = isset($_GET["url"]) ? $_GET["url"] : "/";
+
+// echo  $_SERVER['REQUEST_URI'];
+
+$route = new RouteController($url);
+$route->show();
+
+/**
+ * Footer includes infomation about group, contact and other
+ */
+// include ROOT . DS . 'mvc' . DS . 'views' . DS . 'footer.php';
