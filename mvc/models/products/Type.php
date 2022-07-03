ae2585ab49870a0230284365c3c2f28ddf96e97f
maivantien1107 <maivantien1107@users.noreply.github.com>
2022-07-03 22:07:06 +0700

    add code

--- mvc/models/products/Type.php
+++ mvc/models/products/Type.php
@@ -4,6 +4,6 @@ class Type {
     public const NONE = 0;
     public const MOUSE = 1;
     public const PC = 2;
-    public const LAPTOP = 3;
+    public const LAP = 3;
     public const ACCESSORS = 4;
 }
